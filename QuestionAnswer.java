3117b314f619e7f97a875b5a45c257a67b13496d
Prerna Deshmane <119517124+Prerana0008@users.noreply.github.com>
2023-09-29 17:12:23 +0530

    QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,20 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String Question[] ={
+            "Q.1. Which is the favourite sport of india?",
+            "Q.2. Who started cricket first?",
+            "Q.3. Which of the following is not programming language? "
+    };
+    public static String choices[][]={
+            {"Cricket","Badminton","Hockey","Football"},
+            {"India","England","US","Japan"},
+            {"Java","Python","C++","Pycharm"}
+
+    };
+    public static String correctAnswer[] = {
+        "Hockey",
+        "England",
+        "Pycharm"
+    };
+}
